5038e7bc2df13bc9875d1a4cfda88b789e86c806
unknown <quanhnph32095@fpt.edu.vn>
2024-02-29 03:20:48 +0700

    case adddm

--- index.php
+++ index.php
@@ -240,7 +240,7 @@ if (isset($_GET['act']) && ($_GET['act'] != "")) {
             break;
 
        
-      
+        
         default:
             include "font_end/home.php";
             break;
